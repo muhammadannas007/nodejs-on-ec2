ec940fb424b2bb7464d79e117ad7001b34411e3d
muhammadannas007 <110085281+muhammadannas007@users.noreply.github.com>
2024-01-10 16:37:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 //create a server object:
 http.createServer(function (req, res) {
 
-  res.write('A Monk in a Code Frenetics'); //write a response to the client
+  res.write('A Monk in a devops'); //write a response to the client
 
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
