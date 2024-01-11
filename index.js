7b3532cde7bde47a2fde9934f28cc664dbe7fe29
muhammadannas007 <110085281+muhammadannas007@users.noreply.github.com>
2024-01-11 17:13:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http');
 http.createServer(function (req, res) {
 
 
-  res.write('A Monk in a Devops'); //write a response to the client
+  res.write('A Monk in a QA'); //write a response to the client
 
 
   res.end(); //end the response
