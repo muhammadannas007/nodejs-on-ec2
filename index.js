314b15023a16f6b4e181d35bd195351db13acfb9
muhammadannas007 <110085281+muhammadannas007@users.noreply.github.com>
2024-01-18 15:08:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ http.createServer(function (req, res) {
   res.write('A Monk in a Deveeeeeee'); //write a response to the client
 
   res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+}).listen(3000); //the server object listens on port 80
 
