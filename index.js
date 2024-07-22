0f0e833ab2511441da347f6e2c3b19c9e8d9e486
muhammadannas007 <110085281+muhammadannas007@users.noreply.github.com>
2024-07-22 16:21:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ res.write('A Monk in a MASTER'); //write a response to the client
 
 
   res.end(); //end the response
-}).listen(3000); //the server object listens on port 80
+}).listen(3000); //the server object listens on port 80   hahahahadone
 
